d31b140ded897b71cb2ff356923fe6611b04501d
Sarkissian-Telmane <telmane.sarkissian@edu.univ-fcomte.fr>
2026-01-07 00:31:38 +0100

    JS toggle des bandes d'annonces + corrections bugs pour rendre plus responsiv

--- src/js/script.js
+++ src/js/script.js
@@ -10,4 +10,25 @@ if (toggle && nav) {
         toggle.ariaExpanded = isClosed;
         nav.hidden = isOpen;
     });
-}
+};
+
+const scroll = document.querySelector(".cta__button-scroll")
+if (scroll) {
+    scroll.addEventListener("click", () => {
+        window.scroll({ top: 870, left: 0, behavior: "smooth", });
+    });
+};
+
+const buttons = document.querySelectorAll(".cadre__film-btn");
+const videos = document.querySelectorAll(".ba__film");
+
+buttons.forEach((button, index) => {
+    const video = videos[index];
+
+    button.addEventListener("click", () => {
+        const isOpen = button.getAttribute("aria-expanded") === "true";
+
+        button.setAttribute("aria-expanded", String(!isOpen));
+        video.hidden = isOpen;
+    });
+});
