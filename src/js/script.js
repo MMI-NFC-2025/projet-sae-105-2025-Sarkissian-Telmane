36775d6f500e573bcd9c2b0002c52e622780867b
Sarkissian-Telmane <telmane.sarkissian@edu.univ-fcomte.fr>
2026-01-06 15:48:45 +0100

    corrections css header + menu css et js

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+const toggle = document.querySelector(".menu-btn");
+const nav = document.querySelector(".menu");
+
+if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+        const isOpen = toggle.ariaExpanded === "true";
+        const isClosed = !isOpen;
+        document.body.classList.toggle('no-scroll');
+
+        toggle.ariaExpanded = isClosed;
+        nav.hidden = isOpen;
+    });
+}
